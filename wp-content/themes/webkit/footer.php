b9f351f703ced95b7a2628d48cf4ba319acaf817
nass3k <nass3k@gmail.com>
2021-07-14 17:38:23 +0300

    header and footer

--- wp-content/themes/webkit/footer.php
+++ wp-content/themes/webkit/footer.php
@@ -11,7 +11,329 @@
 
 ?>
 
+    <section class="blue">
+        <footer>
+            <div class="container narrow">
+                <div class="row">
+                    <div class="col col-12 col-sm-6 col-lg-3">
+                        <div class="title">Телефон:</div>
+                        <div>
+                            <div>
+                                <a href="tel:+7 (999) 027 60 24">+7 (999) 027 60 24</a>
+                            </div>
+                            <br>
+                        </div>
+                    </div>
+                    <div class="col col-12  col-sm-6 col-lg-3">
+                        <div class="title">Адрес:</div>
+                        <div>
+                            Санкт-Петербург,
+                            <br>ул. Белы Куна, 32                    </div>
+                    </div>
+                    <div class="col col-12  col-sm-6 col-lg-3">
+                        <div class="title">E-mail:</div>
+                        <div>
+                            <a href="mailto:info@webkit.pw">info@webkit.pw</a>
+                        </div>
+                    </div>
+                    <div class="col col-12  col-sm-6 col-lg-3 social-part">
+<!--                        <div class="social-links">-->
+<!--                            <a class="link" href="https://www.instagram.com/itfactory.su/" target="_blank">-->
+<!--                                <svg class="icon inst">-->
+<!--                                    <use xlink:href="/local/templates/itf/assets/icons/sprite.svg#instagram"></use>-->
+<!--                                </svg>-->
+<!--                            </a>-->
+<!--                            <a class="link" href="https://www.facebook.com/ITFactory.su/" target="_blank">-->
+<!--                                <svg class="icon fb">-->
+<!--                                    <use xlink:href="/local/templates/itf/assets/icons/sprite.svg#fb"></use>-->
+<!--                                </svg>-->
+<!--                            </a>-->
+<!--                            <a class="link" href="https://vk.com/itfactory1" target="_blank">-->
+<!--                                <svg class="icon vk">-->
+<!--                                    <use xlink:href="/local/templates/itf/assets/icons/sprite.svg#vk"></use>-->
+<!--                                </svg>-->
+<!--                            </a>-->
+<!--                            <a class="link" href="https://rostov.hh.ru/employer/2897262" target="_blank">-->
+<!--                                <svg class="icon hh" viewBox="0 0 150 150"><circle cy="75" cx="75" r="74.602242" /><text x="22.67857" y="105.83332"><tspan style="font-style:normal;font-variant:normal;font-weight:bold;font-stretch:normal;font-size:88.1944px;font-family:'Century Gothic';-inkscape-font-specification:'Century Gothic Bold';text-align:start;text-anchor:start;" x="22.67857" y="105.83332">hh</tspan></text></svg>-->
+<!--                            </a>-->
+<!--                            <a class="link" href="https://wa.me/79381440303" target="_blank">-->
+<!--                                <svg class="icon whatsapp"><use xlink:href="/local/templates/itf/assets/icons/sprite.svg#whatsapp"></use></svg>-->
+<!--                            </a>-->
+<!---->
+<!--                        </div>-->
+                        <div class="link"><a href="#" target="_blank">Скачать презентацию.pdf</a></div>
+                        <div class="to-top js-to-top">Наверх ↑</div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </section>
 
+</div> <!-- js-scroller -->
+
+<div id="cookies-message" class="tooltip hide">
+    Мы используем cookies для вашего удобства. Пользуясь сайтом, вы соглашаетесь с политикой использования файлов
+    cookies.
+    <a href="privacy-policy.html" target="_blank">Узнать больше</a>
+    <div class="tooltip__close js-cookies-message-close">
+        <svg class="icon close"><use xlink:href="/local/templates/itf/assets/icons/sprite.svg#close"></use></svg>
+    </div>
+</div>
+
+<!-- modals -->
+<div class="feedback-modal blue js-feedback-modal">
+    <div class="close js-close-feedback"></div>
+    <div></div>
+    <div class="container narrow">
+        <div class="form-wrapper">
+            <div class="header-part">
+                <h3 class=" h1 title"><span class="animation-filled animation-delay-1 js-feedback-title">
+                         Обратная связь                        </span></h3>
+                <p class="subtitle"><span class="animation-filled animation-delay-2 js-feedback-subtitle">
+                          Оставьте заявку на просчет вашего проекта:                        </span>
+                </p>
+                <div class="rabbit-wrapper rabbit">
+                    <div class="rabbit-animate">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 109.04 56.18">
+
+                            <path class="fil0" d="">
+                                <animate dur="1.5s" repeatCount="indefinite" attributeName="d"
+                                         values="M62.39,4a2,2,0,1,1,0-4H107a2,2,0,1,1,0,4Z;
+                M88.69,4a2,2,0,1,1,0-4H107a2,2,0,1,1,0,4Z;
+                 M62.39,4a2,2,0,1,1,0-4H107a2,2,0,1,1,0,4Z"
+                                />
+                            </path>
+                            <path class="fil0" d="">
+                                <animate dur="1.5s" repeatCount="indefinite" attributeName="d"
+                                         values="M81.12,28.6a2,2,0,0,1,0-4.05h9.47a2,2,0,0,1,0,4.05Z;
+                 M31.92,28.6a2,2,0,1,1,0-4.05H90.59a2,2,0,0,1,0,4.05Z;
+                 M81.12,28.6a2,2,0,0,1,0-4.05h9.47a2,2,0,0,1,0,4.05Z"
+                                />
+                            </path>
+                            <path class="fil0" d="">
+                                <animate dur="1.5s" repeatCount="indefinite" attributeName="d"
+                                         values="M2,41.72a2,2,0,0,1,0-4H62.3a2,2,0,0,1,0,4Z;
+                 M2,41.72a2,2,0,0,1,0-4H21.74a2,2,0,0,1,0,4Z;
+                M2,41.72a2,2,0,0,1,0-4H62.3a2,2,0,0,1,0,4Z"
+                                />
+                            </path>
+                            <path class="fil0" d="">
+                                <animate dur="1.5s" repeatCount="indefinite" attributeName="d"
+                                         values="M44.7,56.18a2,2,0,0,1,0-4H97.9a2,2,0,1,1,0,4Z;
+                 M65.76,56.18a2,2,0,1,1,0-4H97.9a2,2,0,1,1,0,4Z;
+                 M44.7,56.18a2,2,0,0,1,0-4H97.9a2,2,0,1,1,0,4Z"
+                                />
+                            </path>
+                        </svg>
+                    </div>
+                    <img  class="img js-animation fadeIn" data-wow-duration="1s" data-wow-delay="1s"src="local/templates/itf/assets/images/rabbit-white_real.png" alt="">
+                </div>
+            </div>
+            <form method="post" enctype="multipart/form-data" id="feedbackform2" class="form feedback-form">
+                <input type="hidden" name="city" value="St Petersburg">
+                <input type="hidden" name="lat" value="59.89810">
+                <input type="hidden" name="lon" value="30.26190">
+                <div class="row">
+                    <div class="col col-12 col-md-4 form-group animation-filled animation-delay-3">
+                        <div class="text-field">
+                            <input class="field" type="text" name="name" id="js-name" placeholder="Имя"
+                                   value="">
+                        </div>
+                    </div>
+                    <div class="col col-12 col-md-4 form-group animation-filled animation-delay-4">
+                        <div class="text-field">
+                            <input class="field" id="js-email" name="email" type="email" placeholder="E-mail"
+                                   value="">
+                        </div>
+                    </div>
+                    <div class="col col-12 col-md-4 form-group animation-filled animation-delay-5">
+                        <div class="text-field">
+                            <input class="field" id="phone2" name="phone" onCopy="return false" type="tel"
+                                   value="" required
+                                   placeholder="Телефон*">
+                        </div>
+                    </div>
+                </div>
+                <div class="textarea form-group animation-filled animation-delay-6">
+                    <textarea id="js-comment" name="comments" class="field" placeholder="Комментарий"></textarea>
+                </div>
+
+                <div class="form-bottom row">
+                    <div class="left col col-12 col-md-8 form-group animation-filled animation-delay-7">
+                        <label class="file-input upload">
+                            <svg class="icon">
+                                <use xlink:href="/local/templates/itf/assets/icons/sprite.svg#paperclip"></use>
+                            </svg>
+
+                        </label>
+                        <ul class="upload_res">
+                        </ul>
+                    </div>
+                    <div class="right col col-12 col-md-4 form-group">
+                        <div class="animation-filled animation-delay-8">
+                            <button class="button white full-width" type="submit">Отправить</button>
+                        </div>
+                        <div class="checkbox animation-filled animation-delay-9">
+                            <label>
+                                <input name="check" required type="checkbox" checked="checked">
+                                <span class="checkbox-label">Я согласен с <a href="privacy-policy.html" target="_blank">политикой конфедициальности</a></span>
+                            </label>
+                        </div>
+                    </div>
+                </div>
+
+                <input id="utm_source" type="hidden" name="utm_source"
+                       value="">
+                <input id="utm_medium" type="hidden" name="utm_medium"
+                       value="">
+                <input id="utm_campaign" type="hidden" name="utm_campaign"
+                       value="">
+                <input id="utm_content" type="hidden" name="utm_content"
+                       value="">
+                <input id="utm_term" type="hidden" name="utm_term"
+                       value="">
+
+                <input type="hidden" id="g-recaptcha-response" name="g-recaptcha-response">
+            </form>
+            <div class="success-message">
+                <div class="header-part">
+                    <h2 class="h1">Заявка уже в пути!</h2>
+                    <div class="subtitle">Наш менеджер свяжется с вами, как только ему придет заявка. А пока ожидаете,
+                        вы можете почитать статьи в нашем блоге:
+                    </div>
+                    <div class="buttons">
+                        <a href="news/index.html" class="button white">Перейти к статьям</a>
+                        <!--<a href="/" class="button primary">На главную</a>-->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div></div>
+</div>
+<div class="vacancy-modal blue">
+    <div class="close js-close-vacancy"></div>
+    <div></div>
+    <div class="container narrow">
+        <div class="form-wrapper">
+            <div class="header-part">
+                <h1 class="title"><span class="animation-filled animation-delay-1">Откликнуться на вакансию</span></h1>
+                <p class="subtitle"><span class="animation-filled animation-delay-2"></span></p>
+                <div class="rabbit-wrapper rabbit">
+                    <div class="rabbit-animate">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 109.04 56.18">
+                            <path class="fil0" d="">
+                                <animate dur="1.5s" repeatCount="indefinite" attributeName="d" values="M62.39,4a2,2,0,1,1,0-4H107a2,2,0,1,1,0,4Z;M88.69,4a2,2,0,1,1,0-4H107a2,2,0,1,1,0,4Z;M62.39,4a2,2,0,1,1,0-4H107a2,2,0,1,1,0,4Z"/>
+                            </path>
+                            <path class="fil0" d="">
+                                <animate dur="1.5s" repeatCount="indefinite" attributeName="d" values="M81.12,28.6a2,2,0,0,1,0-4.05h9.47a2,2,0,0,1,0,4.05Z;M31.92,28.6a2,2,0,1,1,0-4.05H90.59a2,2,0,0,1,0,4.05Z;M81.12,28.6a2,2,0,0,1,0-4.05h9.47a2,2,0,0,1,0,4.05Z"/>
+                            </path>
+                            <path class="fil0" d="">
+                                <animate dur="1.5s" repeatCount="indefinite" attributeName="d" values="M2,41.72a2,2,0,0,1,0-4H62.3a2,2,0,0,1,0,4Z;M2,41.72a2,2,0,0,1,0-4H21.74a2,2,0,0,1,0,4Z;M2,41.72a2,2,0,0,1,0-4H62.3a2,2,0,0,1,0,4Z"/>
+                            </path>
+                            <path class="fil0" d="">
+                                <animate dur="1.5s" repeatCount="indefinite" attributeName="d" values="M44.7,56.18a2,2,0,0,1,0-4H97.9a2,2,0,1,1,0,4Z;M65.76,56.18a2,2,0,1,1,0-4H97.9a2,2,0,1,1,0,4Z;M44.7,56.18a2,2,0,0,1,0-4H97.9a2,2,0,1,1,0,4Z"/>
+                            </path>
+                        </svg>
+                    </div>
+                    <img  class="img js-animation fadeIn" data-wow-duration="1s" data-wow-delay="1s"src="local/templates/itf/assets/images/rabbit-white_real.png" alt="">
+                </div>
+            </div>
+            <form method="post" enctype="multipart/form-data" id="vacancy-form" class="form feedback-form js-vacancy-form">
+                <input type="hidden" name="city" value="St Petersburg">
+                <input type="hidden" name="lat" value="59.89810">
+                <input type="hidden" name="lon" value="30.26190">
+                <label class="select form-group animation-filled animation-delay-3">
+                    <select disabled name="vacancy" id="vacancy" class="js-custom-select">
+                        <option value="React developer">React разработчик</option>
+                        <option value="Kotlin/Java-developer">Kotlin/Java-разработчик</option>
+                        <option value="IOS-developer(Swift)">IOS-разработчик (Swift)</option>
+                        <option value="1C-Bitrix developer">Веб-разработчик на 1С-Битрикс</option>
+                    </select>
+                </label>
+                <div class="row">
+                    <div class="col col-12 col-md-4 form-group animation-filled animation-delay-3">
+                        <div class="text-field">
+                            <input class="field" type="text" name="name" id="js-name" placeholder="Имя"
+                                   value="">
+                        </div>
+                    </div>
+                    <div class="col col-12 col-md-4 form-group animation-filled animation-delay-4">
+                        <div class="text-field">
+                            <input class="field" id="js-email" name="email" type="text" placeholder="E-mail"
+                                   value="">
+                        </div>
+                    </div>
+                    <div class="col col-12 col-md-4 form-group animation-filled animation-delay-5">
+                        <div class="text-field">
+                            <input class="field" id="phone2" name="phone" type="tel"
+                                   value="" required
+                                   placeholder="Телефон">
+                        </div>
+                    </div>
+                </div>
+                <div class="textarea form-group animation-filled animation-delay-6">
+                    <textarea id="js-comment" name="comments" class="field" placeholder="Комментарий"></textarea>
+                </div>
+
+                <div class="form-bottom row">
+                    <div class="left col col-12 col-md-8 form-group animation-filled animation-delay-7">
+                        <label class="file-input upload">
+                            <svg class="icon">
+                                <use xlink:href="/local/templates/itf/assets/icons/sprite.svg#paperclip"></use>
+                            </svg>
+
+                        </label>
+                        <ul class="upload_res">
+                        </ul>
+                    </div>
+                    <div class="right col col-12 col-md-4 form-group">
+                        <div class="animation-filled animation-delay-8">
+                            <button class="button white full-width" type="submit">Отправить</button>
+                        </div>
+                        <div class="checkbox animation-filled animation-delay-9">
+                            <label>
+                                <input name="check" required type="checkbox" checked="checked">
+                                <span class="checkbox-label">Я согласен с <a href="privacy-policy.html" target="_blank">политикой конфедициальности</a></span>
+                            </label>
+                        </div>
+                    </div>
+                </div>
+
+                <input id="utm_source" type="hidden" name="utm_source"
+                       value="">
+                <input id="utm_medium" type="hidden" name="utm_medium"
+                       value="">
+                <input id="utm_campaign" type="hidden" name="utm_campaign"
+                       value="">
+                <input id="utm_content" type="hidden" name="utm_content"
+                       value="">
+                <input id="utm_term" type="hidden" name="utm_term"
+                       value="">
+
+                <input type="hidden" id="g-recaptcha-response" name="g-recaptcha-response">
+            </form>
+
+            <div class="success-message">
+                <div class="header-part">
+                    <h2 class="h1">Заявка уже в пути!</h2>
+                    <div class="subtitle">Наш менеджер свяжется с вами, как только ему придет заявка. А пока ожидаете,
+                        вы можете почитать статьи в нашем блоге:
+                    </div>
+                    <div class="buttons">
+                        <a href="news/index.html" class="button white">Перейти к статьям</a>
+                    </div>                </div>
+            </div>
+        </div>
+    </div>
+    <div></div>
+</div>
+
+
+
+
+<div>
+</div>
 
 <?php wp_footer(); ?>
 
--- wp-content/themes/webkit/header.php
+++ wp-content/themes/webkit/header.php
@@ -23,3 +23,187 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 
+
+
+
+<div id="panel"></div>
+<div class="header dk-home-header dark  ">
+    <div class="container">
+        <div class="container-inner">
+            <a href="index.html" class="logo">
+                <svg class="icon">
+                    <use xlink:href="/local/templates/itf/assets/icons/sprite.svg#logo"></use>
+                </svg>
+            </a>
+
+            <nav class="main-nav">
+
+
+
+
+                <a href="local/gulp/build/assets/images/port/b&amp;amp%3Bw.jpg.html" data-badge="" class="link">Портфолио</a>
+
+
+
+
+
+                <div class="link parent">
+                    <span>Услуги</span>
+                    <div class="child-items">
+
+
+
+
+
+
+                        <a href="services/veb-razrabotka-na-1s-bitriks/index.html" data-badge="" class="link">Веб-разработка</a>
+
+
+
+
+
+                        <a href="services/razrabotka-prilozheniy/index.html" data-badge="" class="link">Разработка мобильных приложений</a>
+
+
+
+                    </div></div>
+
+                <a href="about/index.html" data-badge="4" class="link">Вакансии</a>
+
+
+
+
+
+                <a href="news/index.html" data-badge="" class="link">Блог</a>
+
+
+
+
+
+                <a href="contacts/index.html" data-badge="" class="link">Контакты</a>
+
+
+
+                <div class="nav-track">
+                    <div class="nav-track__active"></div>
+                </div>
+            </nav>
+
+
+            <div class="right-side">
+
+
+                <!--  <div class="language">
+                      <span class="active">Ru</span>
+                      <span>/</span>
+                      <a href="#">En</a>
+                  </div>-->
+
+                <div class="email">info@webkit.pw</div>
+
+                <div class="phone">
+                    <a href="tel:+7 (999) 027 60 24">+7 (999) 027 60 24</a>
+                                    </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="hamburger js-navigation-open">
+    <div class="line line-1"></div>
+    <div class="line line-2"></div>
+    <div class="line line-3"></div>
+</div>
+<div class="overlay-background"></div>
+<div class="overlay-navigation">
+    <div class="container narrow">
+        <div class="top">
+
+            <div class="left">
+
+
+
+
+                <a class="h1 link animation-element animation-delay-1" href="local/gulp/build/assets/images/port/b&amp;amp%3Bw.jpg.html"><span>Портфолио</span></a>
+
+
+
+
+
+
+                <div class="h1 link animation-element animation-delay-2"><span>Услуги</span></div>
+
+
+
+
+
+
+                <a href="services/veb-razrabotka-na-1s-bitriks/index.html" class="link-child animation-element animation-delay-5"><span>Веб-разработка</span></a>
+
+
+
+
+
+                <a href="services/razrabotka-prilozheniy/index.html" class="link-child animation-element animation-delay-5"><span>Разработка мобильных приложений</span></a>
+
+
+
+
+
+                <a class="h1 link animation-element animation-delay-1" href="about/index.html"><span>Вакансии</span></a>
+
+
+
+
+
+                <a class="h1 link animation-element animation-delay-1" href="news/index.html"><span>Блог</span></a>
+
+
+
+
+
+                <a class="h1 link animation-element animation-delay-1" href="contacts/index.html"><span>Контакты</span></a>
+
+
+
+
+
+            </div>
+
+
+            <div class="right">
+                <div class="feedback-button js-feedback-open">
+                    <span class="button-text">Оставить заявку</span>
+                </div>
+            </div>
+        </div>
+        <div class="bottom animation-element animation-delay-10">
+
+            <div>
+                <a href="tel:tel:+7 (999) 027 60 24"> tel:+7 (999) 027 60 24                </a>
+            </div>
+            <div>
+                <a href="mailto:info@webkit.pw">
+                    info@webkit.pw                </a>
+            </div>
+            <div>
+                Санкт-Петербург, ул. Белы Куна, 32
+            </div>
+
+<!--            <div>-->
+<!--                <div class="language">-->
+<!--                     <span class="active">Ru</span>-->
+<!--                     <span>/</span>-->
+<!--                     <a href="#">En</a>-->
+<!--                 </div>-->
+<!--            </div>-->
+            <div class="social-links">
+
+                <a class="link" href="https://api.whatsapp.com/send/?phone=79990276024&text=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5%2C+%D0%9D%D0%B8%D0%BA%D0%B8%D1%82%D0%B0%21+%D0%A3+%D0%BC%D0%B5%D0%BD%D1%8F+%D0%B5%D1%81%D1%82%D1%8C+%D0%B2%D0%BE%D0%BF%D1%80%D0%BE%D1%81%D1%8B+%D0%BF%D0%BE+%D0%B2%D0%B5%D0%B1-%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%BA%D0%B5." target="_blank">
+
+                    <img src="<?= get_template_directory_uri(); ?>/assets/img/wa.png" alt="" class="icon whatsapp">
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
